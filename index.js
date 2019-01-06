4a1e4368a884ce542a7724fdbae6f2dc7069d95e
ziyadmerhebi <ziyadmerhebi@gmail.com>
2019-01-05 21:10:51 -0600

    Join voice channels and plays YouTube video

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//test
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setActivity('J Cole', {type: 'LISTENING'}); 
+});
+
+process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+
+client.on('message', message => {
+    if (message.content === '!music') {
+        if (message.channel.type !== 'text') return;
+
+        const { voiceChannel } = message.member;
+
+        if (!voiceChannel) {
+            return message.reply('please join a voice channel first!');
+        }
+
+        voiceChannel.join().then(connection => {
+            const stream = ytdl('https://www.youtube.com/watch?v=UZwyUC4gloM', { filter: 'audioonly' });
+            const dispatcher = connection.playStream(stream);
+
+            dispatcher.on('end', () => voiceChannel.leave());
+        });
+    }
+});
+
+client.login('YOUR-TOKEN-HERE');
